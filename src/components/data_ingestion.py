a19ac23fa657851419222156823a2353247c3268
Sejal-patel <sejal9992@gmail.com>
2023-05-30 14:38:38 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,11 @@
 import os
 import sys
+sys.path.insert(0, os.path.abspath('src'))
 from src.exception import CustomException
-from src.logging import logging
+from src.logger import logging
 import pandas as pd
 
-from sklear.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 @dataclass
@@ -18,14 +19,38 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
-    def initiate_data_ingestion():
-        logging.info("")
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
 
         try:
-            df = pd.read_csv('\notebook\data\stud.csv')
+            # reading the raw data from any source you want such as DB, API, cloud
+            df = pd.read_csv('notebook\data\stud.csv')
             logging.info("Read the dataset as dataframe")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),event_ok = True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok = True)
             
-        except:
-            pass
+            df.to_csv(self.ingestion_config.raw_data_path, index=False,header=True)
+            
+            logging.info('Train test split initiated')
+            train_set,test_set, = train_test_split(df,test_size = 0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as ex:
+            raise CustomException(ex,sys)
+        
+if __name__ == "__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+    # print(str(sys.path))
+    # print(os.path.abspath('src'))
+    # print(os.path.relpath(os.path.abspath('src')))
+            
--- src/exception.py
+++ src/exception.py
@@ -16,3 +16,5 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+if __name__ == "__main__":
+    print("Hello!!")
